cb5a1f4900d6777b1e8c51099dc7299dc0b6aa79
Anudeep Tippabathuni <120025658+Anudeep007-hub@users.noreply.github.com>
2026-06-02 18:08:25 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:   
+
+        # cost = 0
+        # n = len(prices)
+        # i = 0
+        # for j in range(n):
+        #     if prices[j] - prices[i] < 0:
+        #         i+= 1 
+        #     cost = max(cost, prices[j]-prices[i]) 
+
+        # return cost
+
+        cost = 0 
+        n = len(prices)
+
+        def f(i,j,n): 
+            nonlocal cost 
+            if i >= j:
+                return 
+
+            cost = max(cost ,prices[j] - prices[i]) 
+            f(i+1,j,n) 
+            f(i,j-1,n) 
+
+            return 
+        f(0,n-1,n)
+
+        return cost
+            
+        
